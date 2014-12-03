c152550fdd570b3ae854464f47bec3932ccf52fd
Nat Goodspeed <nat@lindenlab.com>
2014-12-03 10:24:20 -0500

    Defend against a stray unprotected reference to EPOLL_CLOEXEC. Our Linux build host evidently has the epoll_create1() function, but not the EPOLL_CLOEXEC flag the code attempts to pass to it. Test for both before making that call.

--- apr/poll/unix/epoll.c
+++ apr/poll/unix/epoll.c
@@ -332,8 +332,14 @@ static apr_status_t impl_pollcb_create(apr_pollcb_t *pollcb,
                                        apr_uint32_t flags)
 {
     int fd;
-    
-#ifdef HAVE_EPOLL_CREATE1
+
+    /* Not every system that has epoll_create1() also has EPOLL_CLOEXEC
+     * defined. This conditional depends on systems with EPOLL_CLOEXEC defining
+     * it as a macro. We observe that at least one Linux system which defines
+     * EPOLL_CLOEXEC as an enum value also defines it as a macro -- presumably
+     * for exactly this kind of test.
+     */
+#if defined(HAVE_EPOLL_CREATE1) && defined(EPOLL_CLOEXEC)
     fd = epoll_create1(EPOLL_CLOEXEC);
 #else
     fd = epoll_create(size);
