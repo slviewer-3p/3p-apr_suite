c550c72502b8119c2cd93c9839d6f885fe8934fa
Nat Goodspeed <nat@lindenlab.com>
2019-02-15 08:21:05 -0500

    DRTVWR-476: Fix diagnostic output showing PATH.

--- build-cmd.sh
+++ build-cmd.sh
@@ -64,9 +64,9 @@ case "$AUTOBUILD_PLATFORM" in
 from collections import OrderedDict
 print(':'.join(OrderedDict((dir.rstrip('/'), 1) for dir in sys.argv[1].split(':'))))" "$PATH")"
 
-    sep 'PATH'
+    python -c "print(' PATH '.center(72, '='))"
     cygpath -p -m "$PATH" | tr ';' '\n'
-    sep "${#PATH} chars in PATH"
+    python -c "print(' ${#PATH} chars in PATH '.center(72, '='))"
 
     for proj in apr aprutil apriconv xml libapr  libaprutil libapriconv
       do build_sln "apr-util/aprutil.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "$proj"
